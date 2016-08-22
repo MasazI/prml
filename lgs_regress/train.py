208ae3cf9ae06a20d308033795fb2e7256f92567
MasazI <masazdream@gmail.com>
2016-08-23 00:44:29 +0900

    add logistic regression

---
+++ lgs_regress/train.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+import defaultdict
+
+N = 17997
+lr_init = 0.1
+
+def update(W, X, lr):
+    a = sum([W[x] for x in X])
+    g = ((1. / (1. + math.exp(-a))) - 1) if -100. < a else (0. -1)
+    for x in X:
+        W[x] = W[x] - eta*g
+
+
+def train(fi):
+    t = 1
+    W = collections.defaultdict(float)
+    for line in fi:
+        fields = line.strip('\n').split('\t')
+        update(W, fields[1:], float(fields[0]), eta0 / (1+t/float(N)))
+        t += 1
+    return W
