36a77c1785a74102889662262b52cd4919c0b55a
MasazI <masazdream@gmail.com>
2016-08-23 00:53:14 +0900

    修正

--- lgs_regress/train.py
+++ lgs_regress/train.py
@@ -4,11 +4,11 @@ import defaultdict
 N = 17997
 lr_init = 0.1
 
-def update(W, X, lr):
+def update(W, X, l, lr):
     a = sum([W[x] for x in X])
-    g = ((1. / (1. + math.exp(-a))) - 1) if -100. < a else (0. -1)
+    g = ((1. / (1. + math.exp(-a))) - l) if -100. < a else (0. -l)
     for x in X:
-        W[x] = W[x] - eta*g
+        W[x] = W[x] - lr*g
 
 
 def train(fi):
@@ -16,6 +16,6 @@ def train(fi):
     W = collections.defaultdict(float)
     for line in fi:
         fields = line.strip('\n').split('\t')
-        update(W, fields[1:], float(fields[0]), eta0 / (1+t/float(N)))
+        update(W, fields[1:], float(fields[0]), lr_init / (1+t/float(N)))
         t += 1
     return W
